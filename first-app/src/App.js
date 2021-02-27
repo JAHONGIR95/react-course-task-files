7d3e2c49388e556591fdf99281d7d380c80be93c
Jahongir95 <sjahongir0032@gmail.com>
2021-02-28 00:08:09 +0500

    buttons created and imported to the app file

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,6 +1,17 @@
+import { Fragment } from 'react';
+import Button from './companents/Buttons/button';
+
 function App() {
   return (
-    <h1>Hellooou world</h1>
+    <Fragment>
+      <h1>Hellooou world</h1>
+      <h2>Salom Dunyo</h2>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+    </Fragment>
   );
 }
 
---
+++ first-app/src/companents/Buttons/button.js
@@ -0,0 +1,9 @@
+const Button = () => {
+    return(
+        <button>
+            <h1>Salom chnopka</h1>
+        </button>
+    )
+}
+
+export default Button;
