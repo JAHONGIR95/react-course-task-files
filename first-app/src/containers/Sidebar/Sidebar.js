590dc4151ec017b4046b470c1ba74553a603694a
Jahongir95 <sjahongir0032@gmail.com>
2021-03-03 00:19:22 +0500

    sidebar of the project is finished

--- first-app/src/containers/Sidebar/Sidebar.js
+++ first-app/src/containers/Sidebar/Sidebar.js
@@ -1,39 +1,38 @@
 import {
-    ChartIcon,
-    TicketsIcon,
-    IdeasIcon,
-    ContactsIcon,
-    AgentsIcon,
-    ArticlesIcon,
-    SettingsIcon,
-    SubsIcon,
-} from '../../Assets/Icons/Icons';
+  ChartIcon,
+  TicketsIcon,
+  IdeasIcon,
+  ContactsIcon,
+  AgentsIcon,
+  ArticlesIcon,
+  SettingsIcon,
+  SubsIcon,
+} from "../../Assets/Icons/Icons";
 
-import SidebarBtn from '../../companents/SidebarBtn/SidebarBtn';
+import SidebarBtn from "../../companents/SidebarBtn/SidebarBtn";
 
-import MainLogo from '../../Assets/images/icons/logo.png';
+import MainLogo from "../../Assets/images/icons/logo.png";
 
-import './main.scss';
+import "./main.scss";
 
 const Sidebar = () => {
-    return(
-        <div className="sidebar">
-
-            <div className="logo-holder">
-                <img src={MainLogo} alt="logo"/>
-                <a href="javascript:void(0)">Dashboard Kit</a>
-            </div>
-           <SidebarBtn title="Overview" icon={<ChartIcon/>}/>
-           <SidebarBtn title="Tickets" icon={<TicketsIcon/>}/>
-           <SidebarBtn title="Ideas" icon={<IdeasIcon/>}/>
-           <SidebarBtn title="Contacts" icon={<ContactsIcon/>}/>
-           <SidebarBtn title="Agents" icon={<AgentsIcon/>}/>
-           <SidebarBtn title="Articles" icon={<ArticlesIcon/>}/>
-           <SidebarBtn title="Settings" icon={<SettingsIcon/>}/>
-           <SidebarBtn title="Subscription" icon={<SubsIcon/>}/>
-           <h1>Sidebar</h1>
-        </div>
-    );
-}
+  return (
+    <div className="sidebar">
+      <div className="logo-holder">
+        <img src={MainLogo} className="sidebar-logo" alt="logo" />
+        <a href="javascript:void(0)">Dashboard Kit</a>
+      </div>
+      <SidebarBtn title="Overview" icon={<ChartIcon />} />
+      <SidebarBtn title="Tickets" icon={<TicketsIcon />} />
+      <SidebarBtn title="Ideas" icon={<IdeasIcon />} />
+      <SidebarBtn title="Contacts" icon={<ContactsIcon />} />
+      <SidebarBtn title="Agents" icon={<AgentsIcon />} />
+      <SidebarBtn title="Articles" icon={<ArticlesIcon />} />
+      <SidebarBtn title="Settings" icon={<SettingsIcon />} />
+      <SidebarBtn title="Subscription" icon={<SubsIcon />} />
+      <h1>Sidebar</h1>
+    </div>
+  );
+};
 
 export default Sidebar;
