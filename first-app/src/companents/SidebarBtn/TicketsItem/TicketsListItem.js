0270e25de747a55972bf9887636786350cce1989
Jahongir95 <sjahongir0032@gmail.com>
2021-03-04 18:37:08 +0500

    tickets list is developing

---
+++ first-app/src/companents/SidebarBtn/TicketsItem/TicketsListItem.js
@@ -0,0 +1,24 @@
+import './main.scss';
+
+const TicketsItem = ({avatar, clientTitle, clientStatus, clientName, clientDate, clientDay, clientTime, priority}) => {
+    return(
+        <div className="tickets-list-item">
+            <img src={avatar} className="client-image"/>
+            <div className="client-status">
+                <h2 className="client-top-info">{clientTitle}</h2>
+                <span className="client-next-info">{clientStatus}</span>
+            </div>
+            <div className="client-name">
+                <h2 className="client-top-info">{clientName}</h2>
+                <span className="client-next-info">{clientDate}</span>
+            </div>
+            <div className="client-date">
+                <h2 className="client-top-info">{clientDay}</h2>
+                <span className="client-next-info">{clientTime}</span>
+            </div>
+            <div className="priority">
+                <h2 className="priority-case">{priority}</h2>
+            </div>
+        </div>
+    )
+}
