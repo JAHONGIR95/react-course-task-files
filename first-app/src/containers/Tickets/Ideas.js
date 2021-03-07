82b91cfbd65f1f3263dc765709524b1bffa19415
Jahongir95 <sjahongir0032@gmail.com>
2021-03-07 14:30:16 +0500

    onclick event is added to the react

--- first-app/src/containers/Tickets/Ideas.js
+++ first-app/src/containers/Tickets/Ideas.js
@@ -1,6 +1,19 @@
+import {useState} from 'react';
+
+
 const Ideas = () => {
+    const [count, setCount] = useState(0);
+
+   
+    
     return(
-        <h1>Ideas page</h1>
+        <>
+            <h1>Ideas page</h1>
+            <h1>Son: {count}</h1>
+
+            <button onClick={decriment}>-</button>
+            <button onClick={incriment}>+</button>
+        </>
     )
 }
 
