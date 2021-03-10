0c2a5f0aecd04b17c4afaeb89afec16d5665544d
Jahongir95 <sjahongir0032@gmail.com>
2021-03-10 13:39:07 +0500

    single movie page is developing

---
+++ movie-portal/src/components/GenresCard/GenresCard.js
@@ -0,0 +1,9 @@
+import './GenresCard.scss';
+
+const GenresCard = ({Janri}) => {
+    return (
+        <h4 className="movie-genre">{Janri}</h4>
+    )
+}
+
+export default GenresCard;
--- movie-portal/src/pages/SingleMovie.js
+++ movie-portal/src/pages/SingleMovie.js
@@ -1,42 +1,78 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-const SingleMovie = ({match}) => {
+import GenresCard from '../components/GenresCard/GenresCard';
 
-    const [MovieInfo, setMovieInfo] = useState({
-        isFetched: false,
-        data: [],
-        error: null,
-    })
-  
-    useEffect(() => {
-      axios.get(`https://api.themoviedb.org/3/movie/${match.params.id}`, {
-          params: {
-              api_key: 'd2a8ca5d342a4ac27541b9319d594c83'
-          }
-        })
-        .then(function (response) {
-          setMovieInfo({
-              isFetched: true,
-              data: response.data,
-              error: false,
-          })
-        })
-        .catch(function (error) {
-          setMovieInfo({
-              isFetched: true,
-              data: [],
-              error: error,
-          })
-        })
-        .then(function () {
-          // always executed
-        })
-    }, []);
+const SingleMovie = ({ match }) => {
+  const [MovieInfo, setMovieInfo] = useState({
+    isFetched: false,
+    data: [],
+    error: null,
+  });
 
-    return(
-        <h1>SingleMovie {MovieInfo.data.title}</h1>
-    )
-}
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/${match.params.id}`, {
+        params: {
+          api_key: "d2a8ca5d342a4ac27541b9319d594c83",
+        },
+      })
+      .then(function (response) {
+        setMovieInfo({
+          isFetched: true,
+          data: response.data,
+          error: false,
+        });
+      })
+      .catch(function (error) {
+        setMovieInfo({
+          isFetched: true,
+          data: [],
+          error: error,
+        });
+      })
+      .then(function () {
+        // always executed
+      });
+  }, []);
+
+  const mData = MovieInfo.data;
+  console.log(mData);
+
+  return (
+    // <h1>SingleMovie {MovieInfo.data.title}</h1>
+    
+    <div className="single-movie-section">
+      <div className="single-movie-top">
+        <div className="single-movie-top-left">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${mData.backdrop_path}`}
+            alt="movie-image"
+          />
+        </div>
+        <div className="single-movie-top-right">
+          <h1 className="movie-title">{mData.title}</h1>
+          <h3 className="movie date">{`Date  ${mData.release_date}`}</h3>
+          <h3 className="movie-duration">{`Duration  ${Math.floor(mData.runtime / 60)} hour ${mData.runtime % 60} minute`}</h3>
+          
+        </div>
+      </div>
+
+      <div className="single-movie-main">
+        <p className="movie-overview">{mData.overview}</p>
+        {/* {
+          mData.genres.map((ganr, index) => (
+            <GenresCard
+              {
+                Janri={ganr.name}
+                key={index}
+              }
+            />
+          ))
+        } */}
+      </div>
+    </div>
+  );
+};
 
 export default SingleMovie;
