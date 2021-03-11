3c4b6c89b19cc96660bac160301d28f7e7f85a4e
Jahongir95 <sjahongir0032@gmail.com>
2021-03-12 00:03:23 +0500

    latest page is finished

--- movie-portal/src/pages/LatestMovie.js
+++ movie-portal/src/pages/LatestMovie.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
+import MovieCard from '../components/MovieCard';
+
 import './pages.scss';
 
 const LatestMovie = () => {
 
-    const [topMovies, setTopMovies] = useState({
+    const [latestMovie, setLatestMovie] = useState({
         isFetched: false,
         data: [],
         error: null,
@@ -13,21 +15,20 @@ const LatestMovie = () => {
     
       useEffect(() => {
         axios
-          .get("https://api.themoviedb.org/3/movie/top_rated", {
+          .get("https://api.themoviedb.org/3/movie/latest", {
             params: {
               api_key: "d2a8ca5d342a4ac27541b9319d594c83",
             },
           })
           .then(function (response) {
-            //   console.log(response)
-            setTopMovies({
+            setLatestMovie({
               isFetched: true,
               data: response.data,
               error: false,
             });
           })
           .catch(function (error) {
-            setTopMovies({
+            setLatestMovie({
               isFetched: true,
               data: [],
               error: error,
@@ -38,8 +39,38 @@ const LatestMovie = () => {
           });
       }, []);
 
+      console.log(latestMovie.data.length);
+
     return (
-        <h1>LatestMovie</h1>
+        <div className="latest-movie">
+            <div className="container">
+                {
+                    latestMovie.data.length > 0 ? (
+                        latestMovie.isFetched ? (
+                            <div className="movie-list">
+                                {
+                                    latestMovie.data.map((movie, index) => (
+                                        <MovieCard
+                                          id={movie.id}
+                                          title={movie.title}
+                                          imgLink={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                          rating={movie.vote_average}
+                                          releaseDate={movie.release_date}
+                                          key={index}
+                                        />
+                                    ))
+                                }
+                            </div>
+                        ) : (
+                            <p>Loading...</p>
+                        )
+                    ) : (
+                        <h1>Sorry Movie is not found</h1>
+                    )
+                }
+            </div>
+            <h1>LatestMovie</h1>
+        </div>
     )
 }
 
--- movie-portal/src/pages/TopMovies.js
+++ movie-portal/src/pages/TopMovies.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-import MovieCard from '../components/MovieCard'
+import MovieCard from '../components/MovieCard';
 
 import "./pages.scss";
 
