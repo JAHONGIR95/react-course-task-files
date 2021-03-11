74fc628dba619b3cb80647668ae3cb5f39261d8b
Jahongir95 <sjahongir0032@gmail.com>
2021-03-12 00:14:38 +0500

    axios is linked to the TV shows page to developed this page

--- movie-portal/src/pages/TvShow.js
+++ movie-portal/src/pages/TvShow.js
@@ -1,3 +1,10 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import MovieCard from '../components/MovieCard';
+
+import "./pages.scss";
+
 const TvShow = () =>{
     return(
         <h1>TvShow</h1>
