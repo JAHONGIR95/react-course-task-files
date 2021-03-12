190df8ac22895dcc989cf7384e7bfcf15015485f
Jahongir95 <sjahongir0032@gmail.com>
2021-03-13 00:20:49 +0500

    footer page is created

---
+++ movie-portal/src/containers/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="container">
+                
+                <h1>Footer</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
