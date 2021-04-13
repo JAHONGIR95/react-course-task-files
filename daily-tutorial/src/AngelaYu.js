0a521fa89edc308df8cbdf3c59800e427283e688
Jahongir95 <sjahongir0032@gmail.com>
2021-04-13 21:04:39 +0500

    New youtube clone project is started

--- daily-tutorial/src/AngelaYu.js
+++ daily-tutorial/src/AngelaYu.js
@@ -3,10 +3,16 @@ import React from 'react'
 export default function AngelaYu() {
 
     let time = new Date().getHours();
+    let currentTime;
+    {
+        if(time >= 0 && time < 19){
+            currentTime = 'Hayrli tong!';
+        } 
+    }
 
     return (
         <div>
-            <h1>{time}</h1>
+            <h1>{currentTime} {time}</h1>
         </div>
     )
 }
---
+++ youtube-clone/src/App.js
@@ -0,0 +1,12 @@
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <h1>App.js file</h1>
+    </div>
+  );
+}
+
+export default App;
